fa86351177fa9e65b7160d56fd5d42b178441b28
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-03-26 18:53:53 +0300

    Добавлен класс PostVkManager

---
+++ src/main/java/ru/netology/manager/PostVkManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostVk;
+
+public class PostVkManager {
+    private PostVk[] objects;
+
+    public PostVk[] search(int ownerId, boolean ownersOnly, int count, int offset) {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+}
